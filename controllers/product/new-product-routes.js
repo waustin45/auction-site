e11f85c48e3cdc10f573e46d0ef11edac4632445
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-02-09 21:46:57 -0500

    added Bid model and product routes

--- controllers/product/new-product-routes.js
+++ controllers/product/new-product-routes.js
@@ -1,6 +1,5 @@
-
-
 // // localhost3001/product/newproduct
 // model.get("/", )
 // // localhost3001/product/newproduct/create
 // model.get('/create')
+
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Product = require('./Product');
+const Bid = require('./Bid');
+
+module.exports = { User, Product, Bid }
