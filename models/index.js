d21b9e7c24c7bc33667491384fd127c80958f9f5
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-02-10 11:59:00 -0500

    model connections

--- models/index.js
+++ models/index.js
@@ -2,4 +2,28 @@ const User = require('./User');
 const Product = require('./Product');
 const Bid = require('./Bid');
 
+User.hasMany(Product, {
+    foreignKey: 'user_id',
+});
+
+Product.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+User.hasMany(Bid, {
+    foreignKey: 'user_id',
+});
+
+Bid.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Product.hasMany(Bid, {
+    foreignKey: 'product_id',
+});
+
+Bid.belongsTo(Product, {
+    foreignKey: 'product_id',
+});
+
 module.exports = {User, Product, Bid }
