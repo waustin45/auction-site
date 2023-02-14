4bfe995e4161cc78a7bfc4b8ee4f1aaa46ebd1fd
waustin45 <108015666+waustin45@users.noreply.github.com>
2023-02-14 11:22:15 -0500

    to pull

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,7 @@ router.get('/', async (req, res) => {
 
     try {
      const data = await Product.findAll({
+      
         include: [
           {
             model: User,
@@ -94,7 +95,45 @@ router.get('/sort/alphabetical/dsc', async (req, res) => {
     )
    const plainData = data.map((info) => info.get({ plain: true }));
    console.log(plainData)
-   res.render('homepage', { plainData, logged_in: req.session.logged_in, session_user: req.session.user_id  });
+   res.json(plainData)
+  //  res.render('homepage', { plainData });
+  } catch (err) {
+      res.status(500).json(err)
+  }
+  
+
+});
+router.get('/sort/bids/dsc', async (req, res) => {
+
+  try {
+   const data = await Product.findAll({
+   
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+        {
+          model: Bid,
+          attributes: ['bid_amt'],
+         
+          separate: true,
+          order: [['bid_amt', 'DESC']],
+        }
+      ],
+  
+    //    order: [
+    //   [
+    //     "bids", "DESC"
+    //   ]
+    // ],
+    },
+   
+    )
+   const plainData = data.map((info) => info.get({ plain: true }));
+   
+   res.json(plainData)
+  //  res.render('homepage', { plainData, logged_in: req.session.logged_in, session_user: req.session.user_id  });
   //  res.render('homepage', { plainData });
   } catch (err) {
       res.status(500).json(err)
